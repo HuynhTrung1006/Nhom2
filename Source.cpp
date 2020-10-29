9de2cd8cb6c1e5e48c6fb044f384896202c988fd
The Anh <trantheanh041@gmail.com>
2020-10-29 11:03:54 +0700

    Tinh dien tich hinh vuong

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+float dt_hinhvuong(int a);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
@@ -33,4 +34,9 @@ int tich(int a,int b)
 float thuong(int a, int b)
 {
 	return a/b;
+}
+
+float dt_hinhvuong(int a)
+{
+	return a*a;
 }
