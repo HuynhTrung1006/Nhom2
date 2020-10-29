93085a79b9afc71a62765226c53de8feebdfa5a6
TSHue <elsword8585@gmail.com>
2020-10-29 11:25:00 +0700

    Tinh chu vi hinh vuong

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+float Chuvihinhvuong(int a);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
@@ -33,4 +34,8 @@ int tich(int a,int b)
 float thuong(int a, int b)
 {
 	return a/b;
+}
+
+float Chuvihinhvuong(int a){
+	return a*4;
 }
