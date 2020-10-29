78b98f1d1c77364052ed802edb3ffa31cb207f98
pnhthuong <hoaithuong3454@gmail.com>
2020-10-29 11:15:21 +0700

    tinh chuvihcn

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+float tinhChuViHcn(int a,int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
@@ -33,4 +34,9 @@ int tich(int a,int b)
 float thuong(int a, int b)
 {
 	return a/b;
+}
+float tinhChuViHcn(int a,int b)
+{
+	return (a+b)*2;
+	
 }
